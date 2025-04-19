b8cbe7e68fe674fc8715d053843de2da607ea256
Dev Gustavo <gustavomoraisgvm@gmail.com>
2025-04-19 13:08:00 -0300

    Adicionar arquivos de projeto.

---
+++ Reserva/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserva.Entities.Exceptions
+{
+     class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
---
+++ Reserva/Entities/Reservation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Reserva.Entities.Exceptions;
+
+namespace Reserva.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Error in reservation: Check-out date must be after check-in");
+
+            }
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException( "Error in reservation: Reservation dates for update must be future dates");
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Error in reservation: Check-out date must be after check-in");
+
+            }
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room " 
+                + RoomNumber 
+                + ", checkin: " 
+                + CheckIn.ToString("dd/MM/yyyy")
+                +", checkout: "
+                +CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                +" nights";
+        }
+    }
+}
